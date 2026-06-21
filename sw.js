bd9c3764ead1a091bd5e110235720b6d9c15fa25
JRK <jrk@MacBook-Pro-JRK.local>
2026-06-22 01:01:48 +0300

    Update studio name to Todes

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tantsuy-mvp-20260622-6";
+const CACHE_NAME = "tantsuy-mvp-20260622-7";
 const APP_SHELL = [
   "./",
-  "./index.html?v=20260622-6",
-  "./styles.css?v=20260622-6",
-  "./app.js?v=20260622-6",
+  "./index.html?v=20260622-7",
+  "./styles.css?v=20260622-7",
+  "./app.js?v=20260622-7",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
   "./assets/dance-lesson-thumb.png"
