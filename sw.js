a10deccc263dc558645e97082bf4635d82d15020
JRK <jrk@MacBook-Pro-JRK.local>
2026-06-22 16:18:46 +0300

    Refine teacher workspace folder model

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tantsuy-mvp-20260622-10";
+const CACHE_NAME = "tantsuy-mvp-20260622-11";
 const APP_SHELL = [
   "./",
-  "./index.html?v=20260622-10",
-  "./styles.css?v=20260622-10",
-  "./app.js?v=20260622-10",
+  "./index.html?v=20260622-11",
+  "./styles.css?v=20260622-11",
+  "./app.js?v=20260622-11",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
   "./assets/dance-lesson-thumb.png"
